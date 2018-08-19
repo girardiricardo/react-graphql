403d5201079c2f826813d29e5d36181f9393ef09
Ricardo Girardi <girardiricardo@outlook.com>
2018-08-19 17:29:16 -0300

    Modifying Chat component

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
 import { 
     View,
     Text,
@@ -7,6 +11,7 @@ import {
     StyleSheet,
     Dimensions,
     StatusBar,
+    ActivityIndicator,
     KeyboardAvoidingView
 } from 'react-native';
 
@@ -14,61 +19,94 @@ import Input from './components/Input';
 
 StatusBar.setBarStyle('light-content');
 
-const Chat = () => (
-    <KeyboardAvoidingView 
-        style={styles.container}
-        behavior={'padding'}>
-        <ScrollView contentContainerStyle={styles.conversation}>
-            <View style={[styles.bubble, styles['bubble-left']]}>
-                <Text style={styles.author}>Ricardo Girardi</Text>
-                <Text style={styles.message}>Olá amigos!</Text>
+const author = 'Ricardo';
+
+class Chat extends Component {
+    componentDidMount() {
+        this.props.conversation.subscribeToMore({
+            document: gql`
+                subscription onMessageAdded($author: String!) {
+                    Message(filter: {
+                        mutation_in: [CREATED]
+                        node: {
+                            from_not: $author
+                        }
+                    }) { 
+                            node {
+                                id
+                                from
+                                message
+                            }
+                        }
+                }
+            `,
+            variables: {
+                author,
+            },
+            updateQuery: (prev, { subscriptionData }) => {
+                if(!subscriptionData.data['Message']) return prev;
+
+                const newItem = subscriptionData.data['Message'].node;
+
+                return { ...prev, allMessages: [ ...prev.allMessages, newItem ] };
+            }
+        });
+    }
+
+    componentDidUpdate() {
+        setTimeout(() => {
+            this._scrollView.scrollToEnd();
+        }, 0);
+    };
+
+    handleAddMessage = (proxy, { data: { createMessage } }) => {
+        const data = proxy.readQuery({
+            query: ConversationQuery,
+        });
+
+        data.allMessages.push(createMessage);
+
+        proxy.writeQuery({
+            query: ConversationQuery,
+            data
+        });
+    };
+
+    renderChat = () => (
+        this.props.conversation.allMessages.map(item => (
+            <View
+                key={item.id}
+                style={[
+                    styles.bubble, 
+                    item.from === author ? styles['bubble-right'] : styles['bubble-left']
+                ]}
+                >
+                <Text style={styles.author}>{item.from}</Text>
+                <Text style={styles.message}>{item.message}</Text>
             </View>
+        ))
+    );
 
-            <View style={[styles.bubble, styles['bubble-left']]}>
-                <Text style={styles.author}>Ricardo Girardi</Text>
-                <Text style={styles.message}>Olá amigos!</Text>
-            </View>
-
-            <View style={[styles.bubble, styles['bubble-right']]}>
-                <Text style={styles.author}>Ricardo Girardi</Text>
-                <Text style={styles.message}>Olá amigos!</Text>
-            </View>
-
-            <View style={[styles.bubble, styles['bubble-left']]}>
-                <Text style={styles.author}>Ricardo Girardi</Text>
-                <Text style={styles.message}>Olá amigos!</Text>
-            </View>
-
-            <View style={[styles.bubble, styles['bubble-left']]}>
-                <Text style={styles.author}>Ricardo Girardi</Text>
-                <Text style={styles.message}>Olá amigos!</Text>
-            </View>
-
-            <View style={[styles.bubble, styles['bubble-right']]}>
-                <Text style={styles.author}>Ricardo Girardi</Text>
-                <Text style={styles.message}>Olá amigos!</Text>
-            </View>
-
-            <View style={[styles.bubble, styles['bubble-left']]}>
-                <Text style={styles.author}>Ricardo Girardi</Text>
-                <Text style={styles.message}>Olá amigos!</Text>
-            </View>
-
-            <View style={[styles.bubble, styles['bubble-left']]}>
-                <Text style={styles.author}>Ricardo Girardi</Text>
-                <Text style={styles.message}>Olá amigos!</Text>
-            </View>
-
-            <View style={[styles.bubble, styles['bubble-right']]}>
-                <Text style={styles.author}>Ricardo Girardi</Text>
-                <Text style={styles.message}>Olá amigos!</Text>
-            </View>
-
-        </ScrollView>
-
-        <Input />
-    </KeyboardAvoidingView>
-);
+    render() {
+        return(
+            <KeyboardAvoidingView 
+                style={styles.container}
+                behavior={'padding'}
+            >
+                <ScrollView 
+                    contentContainerStyle={styles.conversation}
+                    ref={scrollView => this._scrollView = scrollView}
+                >
+                    { this.props.conversation.loading 
+                        ? <ActivityIndicator style={styles.loading} color="#FFF" />
+                        : this.renderChat() 
+                    }
+                </ScrollView>
+                <Input author={author} onAddMessage={this.handleAddMessage} />
+            </KeyboardAvoidingView>
+        );
+    }
+}
 
 const { width } = Dimensions.get('window');
 
@@ -119,10 +157,23 @@ const styles = StyleSheet.create({
     message: {
         fontSize: 16,
         color: '#333',
+    },
+
+    loading: {
+        marginTop: 10,
     }
-
-
-
 });
 
-export default Chat;
+const ConversationQuery = gql`
+    query {
+        allMessages(
+            orderBy: createdAt_ASC
+        ) {
+            id
+            from
+            message
+        }
+    }
+`;
+
+export default graphql(ConversationQuery, { name: 'conversation' })(Chat);
