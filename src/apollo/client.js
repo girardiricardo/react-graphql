c31a8f8fc1a2520e5e8751b39e25209e5e72b2f1
Ricardo Girardi <girardiricardo@outlook.com>
2018-08-19 17:30:52 -0300

    Config WebSocket

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,12 +1,31 @@
+import { split } from 'apollo-link';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { getMainDefinition } from 'apollo-utilities';
 
 const httpLink = new HttpLink({
     uri: 'https://api.graph.cool/simple/v1/cjl09zvkj0h4c0154gymm3d88',
 });
 
+const wsLink = new WebSocketLink({
+    uri: 'wss://subscriptions.graph.cool/v1/cjl09zvkj0h4c0154gymm3d88',
+    options: { reconnect: true },
+});
+
+const link = split(
+    ({ query }) => {
+        const { kind, operation } = getMainDefinition(query);
+        return kind === 'OperationDefinition' && operation === 'subscription';
+    },
+    wsLink,
+    httpLink,
+);
+
 const client = new ApolloClient({
-    link: httpLink,
+    link,
     cache: new InMemoryCache(),
 });
+
+export default client;
